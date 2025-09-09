94d0f32e1c264bfd0cbbeb7390edec2e3b62497f
Anahi Candia S. <143009077+AnahiCandia@users.noreply.github.com>
2025-09-09 15:47:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Color de enlace activo en navegación según sección visible
+const navLinks = document.querySelectorAll('nav a');
+
+window.addEventListener('scroll', () => {
+  let fromTop = window.scrollY + 100;
+
+  navLinks.forEach(link => {
+    const section = document.querySelector(link.hash);
+    if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+});
